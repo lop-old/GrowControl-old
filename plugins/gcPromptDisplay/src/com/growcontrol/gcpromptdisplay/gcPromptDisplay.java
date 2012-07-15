7296e42ae50b65f15a1185fc741d3bc76a8c2760
lorenzo42p <lorenzo42p@fd3849d7-f5de-27ed-727a-bf598a3f1ae9>
2012-07-15 12:12:04 +0000

    finished load config.yml code for gcPromptDisplay plugin
    git-svn-id: http://growcontrol.googlecode.com/svn/trunk@22 fd3849d7-f5de-27ed-727a-bf598a3f1ae9

--- plugins/gcPromptDisplay/src/com/growcontrol/gcpromptdisplay/gcPromptDisplay.java
+++ plugins/gcPromptDisplay/src/com/growcontrol/gcpromptdisplay/gcPromptDisplay.java
@@ -1,21 +1,23 @@
 package com.growcontrol.gcpromptdisplay;
 
 import java.util.HashMap;
+import java.util.List;
 
-import com.growcontrol.gcServer.commands.gcCommand;
+import com.growcontrol.gcServer.config.gcConfig;
 import com.growcontrol.gcServer.logger.gcLogger;
 import com.growcontrol.gcServer.serverPlugin.gcServerPlugin;
-import com.growcontrol.gcServer.serverPlugin.listeners.gcServerPluginListenerCommand;
 import com.growcontrol.gcServer.serverPlugin.listeners.gcServerPluginListenerOutput;
 import com.growcontrol.gcpromptdisplay.PromptPin.PinMode;
 
-public class gcPromptDisplay extends gcServerPlugin implements gcServerPluginListenerCommand, gcServerPluginListenerOutput {
+public class gcPromptDisplay extends gcServerPlugin implements gcServerPluginListenerOutput {
 	private static final String PLUGIN_NAME = "gcPromptDisplay";
 	// logger
 	public static gcLogger log = getLogger(PLUGIN_NAME);
+	// commands listener
+	private CommandsListener commandsListener = new CommandsListener();
 
 	// pin data
-	public HashMap<Integer, PromptPin> outputPins = new HashMap<Integer, PromptPin>();
+	private static HashMap<Integer, PromptPin> outputPins = new HashMap<Integer, PromptPin>();
 
 
 	@Override
@@ -26,12 +28,11 @@ public class gcPromptDisplay extends gcServerPlugin implements gcServerPluginLis
 		registerCommand("promptdisplay")
 			.addAlias("prompt");
 		// register listeners
-		registerListenerCommand(this);
+		commandsListener = new CommandsListener();
+		registerListenerCommand(commandsListener);
 		registerListenerOutput(this);
 		// load configs
-
-		outputPins.put(0, new PromptPin(PinMode.io));
-		outputPins.put(1, new PromptPin(PinMode.pwm));
+		LoadConfig();
 		UpdatePrompt();
 	}
 
@@ -43,9 +44,40 @@ public class gcPromptDisplay extends gcServerPlugin implements gcServerPluginLis
 	}
 
 
-	@Override
-	public boolean onCommand(gcCommand command, String[] args) {
-		return false;
+	// load config.yml
+	private static void LoadConfig() {
+		gcConfig config = gcConfig.loadFile("plugins/gcPromptDisplay", "config.yml");
+		if(config == null) {
+			log.severe("Failed to load config.yml");
+			return;
+		}
+		@SuppressWarnings("unchecked")
+		List<Object> displays = (List<Object>) config.get("Prompt Displays");
+		if(displays == null) {
+			log.severe("Failed to load Prompt Displays from config!");
+			return;
+		}
+		for(Object obj : displays) {
+			try {
+				@SuppressWarnings("unchecked")
+				HashMap<String, Object> display = (HashMap<String, Object>) obj;
+				if(display == null) {
+					log.severe("Failed to parse config!");
+					continue;
+				}
+				int id = (Integer) display.get("Id");
+				String type = (String) display.get("Type");
+				PinMode mode = PromptPin.fromString(type);
+				if(mode == null) {
+					log.warning("Invalid pin mode! "+type);
+					mode = PinMode.DISABLED;
+				}
+				// add pin
+				outputPins.put(id, new PromptPin(mode));
+			} catch(Exception ignore) {
+				log.severe("Failed to parse config!");
+			}
+		}
 	}
 
 
@@ -58,12 +90,10 @@ public class gcPromptDisplay extends gcServerPlugin implements gcServerPluginLis
 		if(outputPins.containsKey(pinNum)) {
 			pin = outputPins.get(pinNum);
 		} else {
-			pin = new PromptPin(PinMode.io);
+			pin = new PromptPin(PinMode.IO);
 			outputPins.put(pinNum, pin);
 		}
 		pin.setState(args[2]);
-//for(String arg : args)
-//log.warning(arg);
 		UpdatePrompt();
 		return true;
 	}

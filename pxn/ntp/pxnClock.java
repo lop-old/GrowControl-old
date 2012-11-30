bd4fa7e85d25aee6bf58be433dc13f57090ca0a9
lorenzo42p <lorenzo42p@fd3849d7-f5de-27ed-727a-bf598a3f1ae9>
2012-11-30 12:50:36 +0000

    rewrote parts of pxnClock class to make it more object oriented
    git-svn-id: http://growcontrol.googlecode.com/svn/trunk@93 fd3849d7-f5de-27ed-727a-bf598a3f1ae9

--- pxn/ntp/pxnClock.java
+++ pxn/ntp/pxnClock.java
@@ -8,64 +8,87 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.growcontrol.gcServer.gcServer;
+import com.poixson.pxnLogger.pxnLogger;
 
 
 public class pxnClock extends Thread {
 
-	private static boolean usingNTP = false;
-	protected static double localOffset = 0.0;
-	protected static double lastChecked = 0.0;
+	protected boolean enabled  = true;
+	protected String timeServer = "ntp.cais.rnp.br";
 
-	// instance thread
-	protected static final pxnClock instance = new pxnClock();
+	protected double localOffset = 0.0;
+	protected double lastChecked = 0.0;
+
+	// instance lock
+	protected final Object threadLock = null;
 
 
-	public static void updateNTP_Blocking() {
-		updateNTP(false);
+	// static update
+	public static double getLocalOffsetNow() {
+		return getLocalOffsetNow(null);
 	}
-	public static void updateNTP_Threaded() {
-		updateNTP(true);
+	public static double getLocalOffsetNow(String timeServer) {
+		pxnClock clock = new pxnClock();
+		if(timeServer != null && !timeServer.isEmpty())
+			clock.setTimeServer(timeServer);
+		clock.update(false);
+		return clock.getLocalOffset();
 	}
-	public static synchronized void updateNTP(boolean threaded) {
-		if(!usingNTP) return;
-		if(instance.isAlive()) return;
+
+
+	// update from time server
+	public void update(boolean enabled, boolean threaded) {
+		setEnabled(enabled);
+		update(threaded);
+	}
+	public void update(String timeServer, boolean threaded) {
+		setTimeServer(timeServer);
+		update(threaded);
+	}
+	public synchronized void update(boolean threaded) {
+		if(!enabled) return;
+		// still running
+		if(this.isAlive()) return;
 		double time = System.currentTimeMillis();
-		if(lastChecked!=0.0 && (time-lastChecked)/1000.0<60.0) return;
+		if(lastChecked!=0.0 && (time-lastChecked)/1000.0 < 60.0) return;
 		lastChecked = time;
-		if(threaded)
-			instance.start();
-		else
-			doTimeQuery();
+		if(threaded) {
+			// run threaded
+			this.start();
+		} else {
+			// run blocking
+			run();
+		}
 	}
 
 
 	// run time query
-	private static synchronized void doTimeQuery() {
-		if(!usingNTP) return;
-		try {
-			DatagramSocket socket;
-			socket = new DatagramSocket();
-			InetAddress address = InetAddress.getByName("ntp.cais.rnp.br");
-			byte[] buf = new ntpMessage().toByteArray();
-			DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 123);
-			ntpMessage.encodeTimestamp(packet.getData(), 40, fromUnixTimestamp());
-			socket.send(packet);
-			socket.receive(packet);
-			ntpMessage msg = new ntpMessage(packet.getData());
-			// calculate local offset
-			double time = System.currentTimeMillis();
-			localOffset = ((msg.receiveTimestamp - msg.originateTimestamp) + (msg.transmitTimestamp - fromUnixTimestamp(time))) / 2.0;
-			gcServer.log.info("Internal time adjusted by "+ (localOffset>0?"+":"") + new DecimalFormat("0.000").format(localOffset) +" seconds");
-			gcServer.log.debug("System time:   "+pxnClock.timestampToString(time/1000.0));
-			gcServer.log.debug("Adjusted time: "+pxnClock.getTimeString());
-			// clean up
-			socket.close();
-			msg = null;
-			packet = null;
-			socket = null;
-			address = null;
-			return;
+	public void run() {
+		if(!enabled) return;
+		synchronized(threadLock) {
+			try {
+				DatagramSocket socket;
+				socket = new DatagramSocket();
+				InetAddress address = InetAddress.getByName(timeServer);
+				byte[] buf = new ntpMessage().toByteArray();
+				DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 123);
+				ntpMessage.encodeTimestamp(packet.getData(), 40, fromUnixTimestamp());
+				socket.send(packet);
+				socket.receive(packet);
+				ntpMessage msg = new ntpMessage(packet.getData());
+				// calculate local offset
+				double time = System.currentTimeMillis();
+				localOffset = ((msg.receiveTimestamp - msg.originateTimestamp) + (msg.transmitTimestamp - fromUnixTimestamp(time))) / 2.0;
+				pxnLogger.log().info("Internal time adjusted by "+ (localOffset>0?"+":"") + new DecimalFormat("0.000").format(localOffset) +" seconds");
+				pxnLogger.log().debug("System time:   "+timestampToString(time/1000.0));
+				pxnLogger.log().debug("Adjusted time: "+getTimeString());
+				// clean up
+				socket.close();
+				msg = null;
+				packet = null;
+				socket = null;
+				address = null;
+				return;
 //double destinationTimestamp = fromUnixTimestamp();
 //double roundTripDelay = (destinationTimestamp-msg.originateTimestamp) - (msg.transmitTimestamp-msg.receiveTimestamp);
 //double localClockOffset = ((msg.receiveTimestamp - msg.originateTimestamp) + (msg.transmitTimestamp - destinationTimestamp)) / 2;
@@ -73,19 +96,16 @@ public class pxnClock extends Thread {
 //System.out.println("Dest. timestamp:     " + NtpMessage.timestampToString(destinationTimestamp));
 //System.out.println("Round-trip delay: " + new DecimalFormat("0.00").format(roundTripDelay*1000) + " ms");
 //System.out.println("Local clock offset: " + new DecimalFormat("0.00").format(localClockOffset*1000) + " ms");
-		} catch (UnknownHostException e) {
-			gcServer.log.exception(e);
-//		} catch (SocketException e) {
-//			GrowControl.log.exception(e);
-//		} catch (IOException e) {
-//			GrowControl.log.exception(e);
-		} catch (Exception e) {
-			gcServer.log.exception(e);
+			} catch (UnknownHostException e) {
+			pxnLogger.log().exception(e);
+//			} catch (SocketException e) {
+//				GrowControl.log.exception(e);
+//			} catch (IOException e) {
+//				GrowControl.log.exception(e);
+			} catch (Exception e) {
+				pxnLogger.log().exception(e);
+			}
 		}
-	}
-	// run threaded
-	public void run() {
-		doTimeQuery();
 	}
 
 
@@ -107,27 +127,36 @@ public class pxnClock extends Thread {
 
 
 	// get fixed time
-	public static double getTimeSeconds() {
+	public double getLocalOffset() {
+		return localOffset;
+	}
+	public double getTimeSeconds() {
 		return getTimeMillis() / 1000.0;
 	}
-	public static long getTimeMillis() {
+	public long getTimeMillis() {
 		return System.currentTimeMillis() + (long)(localOffset * 1000.0);
 	}
-	public static String getTimeString() {
+	public String getTimeString() {
 		return timestampToString(getTimeSeconds());
 	}
 
 
 	// enable/disable NTP
-	public static boolean usingNTP() {
-		return usingNTP;
+	public boolean isEnabled() {
+		return enabled;
 	}
-	public static void setUsingNTP(boolean enabled) {
-		usingNTP = enabled;
-		if(!usingNTP) {
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+		if(!enabled) {
 			localOffset = 0.0;
 			lastChecked = 0.0;
 		}
+	}
+
+
+	public void setTimeServer(String timeServer) {
+		if(timeServer == null || timeServer.isEmpty()) throw new NullPointerException("timeServer host can't be null!");
+		this.timeServer = timeServer;
 	}
 
 

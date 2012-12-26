6a7d3c3a7af377b4c100312f042af555f09758db
lorenzo42p <lorenzo42p@fd3849d7-f5de-27ed-727a-bf598a3f1ae9>
2012-12-26 07:56:09 +0000

    deleting protocol folder/package and unused classes
    git-svn-id: http://growcontrol.googlecode.com/svn/trunk@146 fd3849d7-f5de-27ed-727a-bf598a3f1ae9

--- server/src/com/growcontrol/gcServer/socketServer/protocol/packetControl.java
+++
@@ -1,5 +0,0 @@
-package com.growcontrol.gcServer.socketServer.packets;
-
-public class packetControl extends serverPacket {
-
-}
--- server/src/com/growcontrol/gcServer/socketServer/protocol/packetDevice.java
+++
@@ -1,5 +0,0 @@
-package com.growcontrol.gcServer.socketServer.packets;
-
-public class packetDevice extends serverPacket {
-
-}
